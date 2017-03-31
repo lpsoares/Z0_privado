a91be692fe24ebbfe8688c64ed6b01bd44a57e47
Luciano Pereira Soares <lpsoares@gmail.com>
2017-03-30 23:53:57 -0300

    ajustado diretorio do vunit

--- TestesHW/run.py
+++ TestesHW/run.py
@@ -5,10 +5,10 @@ root = dirname(__file__)
 
 ui = VUnit.from_argv()
 lib = ui.add_library("lib")
-lib.add_source_files(join(root, "Computador","Modulos","PortasLogicas", "*.vhd"))  # source
-lib.add_source_files(join(root, "Computador","Modulos","UnidadeLogicaAritmetica", "*.vhd"))  # source
-lib.add_source_files(join(root, "Computador","Modulos","CircuitosSequenciais", "*.vhd"))   # source
-lib.add_source_files(join(root, "Computador","Testes","PortasLogicas", "*.vhd"))   # test
-lib.add_source_files(join(root, "Computador","Testes","UnidadeLogicaAritmetica", "*.vhd"))   # test
-lib.add_source_files(join(root, "Computador","Testes","CircuitosSequenciais", "*.vhd"))   # test
+lib.add_source_files(join(root, "..","Computador","Modulos","PortasLogicas", "*.vhd"))  # source
+lib.add_source_files(join(root, "..","Computador","Modulos","UnidadeLogicaAritmetica", "*.vhd"))  # source
+lib.add_source_files(join(root, "..","Computador","Modulos","CircuitosSequenciais", "*.vhd"))   # source
+lib.add_source_files(join(root, "..","Computador","Testes","PortasLogicas", "*.vhd"))   # test
+lib.add_source_files(join(root, "..","Computador","Testes","UnidadeLogicaAritmetica", "*.vhd"))   # test
+lib.add_source_files(join(root, "..","Computador","Testes","CircuitosSequenciais", "*.vhd"))   # test
 ui.main()
